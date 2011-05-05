68d6250e30e144591797aa9c6011ba3dd5ab2f46
Lennart Koopmann <lennart@socketfeed.com>
2011-05-05 20:14:25 +0200

    mapping for sublevel navigation higlighting

--- helpers.rb
+++ helpers.rb
@@ -1,8 +1,12 @@
 helpers do
 
   def menu_link(where)
+    mapping = {
+      "/product/gelf" => "product"
+    }
+
     current = request.path_info
-    active = "class='active-main-menu-link'" if (current == "/#{where}" or (current == "/" and where == "home")) or nil
+    active = "class='active-main-menu-link'" if (current == "/#{where}" or (current == "/" and where == "home") or (mapping[current] != nil and mapping[current] == where)) or nil
     "<a href='/#{where}' #{active}>#{where.capitalize}</a>"
   end
 
