db58db1b06badaf5cddd5cbd61fc66adca173b4f
Lennart Koopmann <lennart@socketfeed.com>
2011-11-08 14:51:17 +0100

    have own titles for every page

--- core.rb
+++ core.rb
@@ -5,26 +5,32 @@ require './helpers.rb'
 
 [ '/', '/home' ].each do |route|
   get route do
+    @title = "Free open source self-hosted log management and exception tracking"
     erb :home
   end
 end
 
 get '/about' do
+  @title = "About"
   erb :about
 end
 
 get '/about/gelf' do
+  @title = "About - GELF"
   erb :gelf
 end
 
 get '/support' do
+  @title = "Support"
   erb :support
 end
 
 get '/download' do
+  @title = "Download"
   erb :download
 end
 
 get '/contact' do
+  @title = "Contact"
   erb :contact
 end
