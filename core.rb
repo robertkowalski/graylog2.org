e778f943c2e24c542aa4b3532fe007f15dce7508
Lennart Koopmann <lennart@socketfeed.com>
2011-12-03 00:19:10 +0100

    gelf logging

--- core.rb
+++ core.rb
@@ -1,11 +1,32 @@
 require 'erb'
 require 'uri'
+require 'gelf'
 
 require './helpers.rb'
 
+helpers do
+  def log(what, fields = {})
+    # heroku config:add GELF_HOST=foo.example.org GELF_PORT=12201
+    n = GELF::Notifier.new(ENV['GELF_HOST'], ENV['GELF_PORT'])
+    standard_fields = { :host => "graylog2.org", :facility => "heroku" }
+    n.notify(what, standard_fields.merge(fields))
+  rescue => e
+    puts "Could not log: #{e}"
+  end
+end
+
+after do
+  message = "#{request.env['REQUEST_METHOD'].upcase} #{request.env['PATH_INFO']} [#{response.status}]"
+  log(message,
+    :_http_return_code => response.status,
+    :_ip_address => request.env['REMOTE_ADDR'],
+    :_full_uri => request.env['REQUEST_URI'],
+    :_user_agent => request.env['HTTP_USER_AGENT']
+  )
+end
+
 [ '/', '/home' ].each do |route|
   get route do
-puts "test"
     @title = "Free open source self-hosted log management and exception tracking"
     erb :home
   end
