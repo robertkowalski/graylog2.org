c6e4477e70a0758995c65c1b72b5923a3c084eaa
Lennart Koopmann <lennart@socketfeed.com>
2011-12-23 17:39:34 +0100

    allow trailing slashes in URLs

--- core.rb
+++ core.rb
@@ -25,39 +25,39 @@ after do
   )
 end
 
-[ '/', '/home' ].each do |route|
+[ '/', '/home/?' ].each do |route|
   get route do
     @title = "Free open source self-hosted log management and exception tracking"
     erb :home
   end
 end
 
-get '/about' do
+get '/about/?' do
   @title = "About"
   erb :about
 end
 
-get '/about/gelf' do
+get '/about/gelf/?' do
   @title = "About - GELF"
   erb :gelf
 end
 
-get '/support' do
+get '/support/?' do
   @title = "Support"
   erb :support
 end
 
-get '/download' do
+get '/download/?' do
   @title = "Download"
   erb :download
 end
 
-get '/contact' do
+get '/contact/?' do
   @title = "Contact"
   erb :contact
 end
 
-get '/ohai/0.9.6' do
+get '/ohai/0.9.6/?' do
   authorize!
   erb :ohai_096, :layout => false
 end
